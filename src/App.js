3326f2bb76f0abd010e55d493b1a38648f711090
nungr2oo1 <nunganesh46@gmail.com>
2025-10-17 23:23:46 +0530

    Refactor App component to include Navbar, implement new state management for product selection, and add useEffect for logging multiplication table of selected product

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
 
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('')
-
-  const handleCount = (value) => {
-    if (value <= 10 && value >= 0) {
-      setCount(value)
+  const [mandhu, setMandhu] = useState(0)
+  /*
+    USE EFFECT - TWO PARAMETERS (1. FUNCTION, 2. DEPENDENCY ARRAY)
+  */
+  useEffect(()=>{
+    for(let i=0; i<=10; i++){
+      console.log(`${mandhu} * ${i} = ${i * mandhu}`)
     }
-  }
+  }, [mandhu])
+
+  /*
+    NOTE: NEVER UPDATE THE DEPENDENCY ARRAY VALUE INSIDE THE USE EFFECT FUNCTION
+  */
 
   return (
     <div className="App">
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px'}}>
-        <button onClick={() => handleCount(count + 1)}>Increment</button>
-        <p>{count}</p>
-        <button onClick={() => handleCount(count - 1)}>Decrement</button>
+      <Navbar />
+      <h1>Welcome to my wine shop!</h1>
+      
+      <div style={{display: 'flex', gap: '10px', justifyContent: 'center', alignItems: 'center', margin: '20px'}}>
+        <button onClick={()=> setMandhu(1)} style={{backgroundColor: 'black', color: 'white', padding: '10px', borderRadius: '5px', minWidth: '100px'}}>BD</button>
+        <button onClick={()=> setMandhu(2)} style={{backgroundColor: 'black', color: 'white', padding: '10px', borderRadius: '5px', minWidth: '100px'}}>M2</button>
+        <button onClick={()=> setMandhu(3)} style={{backgroundColor: 'black', color: 'white', padding: '10px', borderRadius: '5px', minWidth: '100px'}}>JD</button>
       </div>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <p>{name}</p>
+
+      {mandhu == 1 ? (
+      <div>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu0Hs88-J1nllS9TeB-gOUX0v_URYiP_rKDA&s' width={100} height={150} />
+        <h2>Black Dog (Nalla kukka)</h2>
+        <p>Rs. 1200</p>
+      </div>
+      ) : null}
+
+      {mandhu == 2 ? (
+      <div>
+        <img src='https://s3.me-central-1.amazonaws.com/catalog.citydrinks.com/offers/addcd175-aa35-477e-b264-0909d2f8e6e6.jpeg' width={100} height={150} />
+        <h2>M2 (Engg Second Sem Ghost)</h2>
+        <p>Rs. 1500</p>
+      </div>
+      ) : null}
+
+      {mandhu == 3 ? (
+      <div>
+        <img src='https://www.theliquorestate.in/wp-content/uploads/2022/06/JD-Tennessee-Honey-Whisky.png' width={100} height={150} />
+        <h2>Jack Daniels</h2>
+        <p>Rs. 1800</p>
+      </div>
+      ) : null}
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Navbar() {
+    return (
+        <div style={{backgroundColor: 'black', color: 'white', padding: '10px'}}>
+            <h1>Wine Shop</h1>
+        </div>
+    )
+}
+
+export default Navbar;

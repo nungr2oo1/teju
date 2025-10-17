ebf0bab4f76d640b619b467c575debfcfefdb137
nungr2oo1 <nunganesh46@gmail.com>
2025-10-17 22:32:11 +0530

    Add axios dependency, update title, and implement counter in App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+
+  const handleCount = (value) => {
+    if (value <= 10 && value >= 0) {
+      setCount(value)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px'}}>
+        <button onClick={() => handleCount(count + 1)}>Increment</button>
+        <p>{count}</p>
+        <button onClick={() => handleCount(count - 1)}>Decrement</button>
+      </div>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <p>{name}</p>
     </div>
   );
 }

058a10de457aa7a2c930fbc425e291b38f96c507
ganeshr-uwc <ganesh@unitedwecare.com>
2025-10-30 00:37:03 +0530

    Add flower selection feature in Home component, integrating new Data module for flower details and updating UI for user interaction.

---
+++ src/DataCenter/Data.js
@@ -0,0 +1,22 @@
+export const Data = [
+    {
+        "name": "Banthi Puvvu",
+        "link": "https://t3.ftcdn.net/jpg/02/85/76/92/360_F_285769215_j9KfS2OZWf3Rv563v01Vr7QwYkmyYdu8.jpg",
+        "description": "Banthi Puvvu (or Banthu Puvvu) translates to Bougainvillea (Bougainvillea spectabilis) in Telugu and refers to the flower known for its vibrant colors and climbing nature. The phrase is used in folk and devotional songs, such as one for the goddess Yellamma, to describe something beautiful and captivating, like a young girl."
+    },
+    {
+        "name": "Malle Puvvu",
+        "link": "https://5.imimg.com/data5/SELLER/Default/2020/8/EC/WY/WQ/4621023/jasmine-500x500.jpg",
+        "description": "Malle Puvvu (or Mulle Puvvu) translates to Jasmine (Jasminum sambac) in Telugu and refers to the flower known for its sweet fragrance and yellow color. The phrase is used in folk and devotional songs, such as one for the goddess Yellamma, to describe something beautiful and captivating, like a young girl."
+    },
+    {
+        "name": "Gulabi",
+        "link": "https://theaffordableorganicstore.com/cdn/shop/files/Rose-Flowers-2.jpg?v=1744630972",
+        "description": "Gulabi (गुलाबी) in Hindi means pink and is also a word used to evoke feelings of femininity, softness, youth, and freshness; it can refer to various shades of pink and is often used by artisans and wearers of textiles, such as in handwoven sarees or phulkari embroidery. Beyond color, Gulabi can also be the name of plants (like the rose), people, or even places, as seen with Gulabi Nagari (Jaipur). "
+    },
+    {
+        "name": "Podhu Thirugudu Puvvu",
+        "link": "https://sirimalle.com/wp-content/uploads/2019/10/sunflower.jpg",
+        "description": "Podhu Thirugudu Puvvu (or Proddu Thirugudu Puvvu) translates to Sunflower (Helianthus annuus) in Telugu and refers to the flower known for turning its face towards the sun. The phrase is used in folk and devotional songs, such as one for the goddess Yellamma, to describe something beautiful and captivating, like a young girl."
+    }
+]
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,60 +1,41 @@
-import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Data } from "../DataCenter/Data";
 
 function Home () {
 
-    const [mandhu, setMandhu] = useState(0)
-    /*
-      USE EFFECT - TWO PARAMETERS (1. FUNCTION, 2. DEPENDENCY ARRAY)
-    */
-    useEffect(()=>{
-      console.log("The value of mandhu is", mandhu)
-    }, [mandhu])
-  
-    /*
-      NOTE: NEVER UPDATE THE DEPENDENCY ARRAY VALUE INSIDE THE USE EFFECT FUNCTION
-    */
-  
+  const [flower, setFlower] = useState(null);
+
+  const handleFlower = (flower) => {
+    setFlower(flower);
+  }
+
     return (
       <div className="App">
-        <Navbar name="Teju Wine Shop" mandhu={mandhu} />
-        <h1>Welcome to my wine shop!</h1>
+        <Navbar name="Teju Wine Shop" />
+        <h1>Welcome to Flower Selection!</h1>
+        
+        <div style={{display: "flex", gap: "10px", justifyContent: "center", alignItems: "center", margin: "20px"}}>
+          {Data.map((item)=>(
+            <div style={{display: "flex", border:"1px solid black", padding: "10px", borderRadius: "10px", alignItems: "center", justifyContent: "center"}} onClick={()=>handleFlower(item)}>
+              <img src={item.link} alt={item.name} width={"100"} height={"100"} />
+              <h2 style={{margin: 0, padding:0}}>{item.name}</h2>
+            </div>
+          ))}
+        </div>
+
+        {flower && (
+          <div key={flower.name}>
+            <img src={flower.link} alt={flower.name} width={"100"} height={"100"} />
+            <h2>{flower.name}</h2>
+            <p>{flower.description}</p>
+          </div>
+        )}
         
         <div style={{display: 'flex', gap: '10px', justifyContent: 'center', alignItems: 'center', margin: '20px'}}>
-          <button onClick={()=> setMandhu(1)} style={{backgroundColor: 'black', color: 'white', padding: '10px', borderRadius: '5px', minWidth: '100px'}}>BD</button>
-          <button onClick={()=> setMandhu(2)} style={{backgroundColor: 'black', color: 'white', padding: '10px', borderRadius: '5px', minWidth: '100px'}}>M2</button>
-          <button onClick={()=> setMandhu(3)} style={{backgroundColor: 'black', color: 'white', padding: '10px', borderRadius: '5px', minWidth: '100px'}}>JD</button>
+          
         </div>
   
-        {mandhu == 1 ? (
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu0Hs88-J1nllS9TeB-gOUX0v_URYiP_rKDA&s' width={100} height={150} />
-          <h2>Black Dog (Nalla kukka)</h2>
-          <p>Rs. 1200</p>
-        </div>
-        ) : null}
-  
-        {mandhu == 2 ? (
-        <div>
-          <img src='https://s3.me-central-1.amazonaws.com/catalog.citydrinks.com/offers/addcd175-aa35-477e-b264-0909d2f8e6e6.jpeg' width={100} height={150} />
-          <h2>M2 (Engg Second Sem Ghost)</h2>
-          <p>Rs. 1500</p>
-        </div>
-        ) : null}
-  
-        {mandhu == 3 ? (
-        <div>
-          <img src='https://www.theliquorestate.in/wp-content/uploads/2022/06/JD-Tennessee-Honey-Whisky.png' width={100} height={150} />
-          <h2>Jack Daniels</h2>
-          <p>Rs. 1800</p>
-        </div>
-        ) : null}
-
-        <Link to="/contact-us">
-            <button>Contact US</button>
-        </Link>
       </div>
     );
 }
